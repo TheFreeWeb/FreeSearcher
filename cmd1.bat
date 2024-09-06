44018fed7136e1b633777c7dafd93df6a7a53e48
WooRepo <150756291+WooRepo@users.noreply.github.com>
2024-09-07 08:37:48 +1200

    Update cmd1.bat

--- cmd1.bat
+++ cmd1.bat
@@ -1,6 +1,8 @@
 @ECHO off
 ECHO Welcome to V1.0 of the FreeSearcher CLI
+Color e
 cd /d %~dp0
 cmd.exe
-echo Get more info at https://github.com/WooRepo/thefreesearcher/wiki
+cd linkdir
+attrib +h *.bat
 pause 
