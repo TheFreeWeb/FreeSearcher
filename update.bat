60a808033352a041b17ba098e3f92bed04cbb20e
WooRepo <150756291+WooRepo@users.noreply.github.com>
2024-09-07 08:29:13 +1200

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,8 +1,11 @@
 @echo off
+echo Checking for updates!
 curl -L https://github.com/TheFreeWeb/freesearcherlinkdir/archive/refs/heads/main.zip > freesearcherlinkdir-main.zip
 tar xf freesearcherlinkdir-main.zip
 Xcopy freesearcherlinkdir-main /d /E /H /C /I /Y 
 rmdir freesearcherlinkdir-main /s /Q
 del freesearcherlinkdir-main.zip /s /Q 
 del README.md /Q
-echo Updated.
+attrib +h *.bat
+echo Update installed
+
