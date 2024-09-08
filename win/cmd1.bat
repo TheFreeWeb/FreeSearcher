42acae6617174381ab7b16d83123cf2ae5d28eaa
WooRepo <150756291+WooRepo@users.noreply.github.com>
2024-09-08 17:21:33 +1200

    Update cmd1.bat

--- win/cmd1.bat
+++ win/cmd1.bat
@@ -1,4 +1,12 @@
 @ECHO off
+AT > NUL
+IF %ERRORLEVEL% EQU 0 (
+    ECHO you are Administrator
+) ELSE (
+    ECHO you are NOT Administrator. Exiting...
+    PING 127.0.0.1 > NUL 2>&1
+    EXIT /B 1
+)
 ECHO Welcome to V1.0 of the FreeSearcher CLI
 Color E
 echo Checking for updates!
