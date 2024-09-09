96d101c573cd9af8a87d468a23259f2089490b82
Xander <82491729+XanderIsBored@users.noreply.github.com>
2024-09-09 19:00:55 +1200

    Added the ability to lookup files on the linux version

--- linux/Main.sh
+++ linux/Main.sh
@@ -7,8 +7,18 @@ yellow="\e[0;33m"
 
 echo -e "${green}updating..."
 ./Update.sh
-echo -e "${yellow}Welcome to V1.4 of the FreeSearcher CLI ${green}Linux Edition"
-echo -e "${yellow}enter your prompt:${reset}"
+
+
+echo -e "${blue}Do you wish to see the availible downloads? [Yy,Nn] ${green}"
+read answer
+
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    find . -type f -name "*.sh"
+fi
+
+
+echo -e "${yellow}Welcome to V1.0 of the FreeSearcher CLI ${blue}Linux Edition"
+echo -e "${yellow}enter your prompt:${blue}"
 
 read location
 
