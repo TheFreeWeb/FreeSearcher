de352aad692be375c52c8af3acd336e737fbc029
WooRepo <150756291+WooRepo@users.noreply.github.com>
2024-09-10 17:35:58 +1200

    Update cmd1.bat

--- win/cmd1.bat
+++ win/cmd1.bat
@@ -2,14 +2,14 @@
 ECHO Welcome to V1.4 of the FreeSearcher CLI
 Color E
 echo Checking for updates!
-curl -L https://github.com/TheFreeWeb/freesearcherlinkdir/archive/refs/heads/main.zip > freesearcherlinkdir-main.zip
-tar xf freesearcherlinkdir-main.zip
-Xcopy freesearcherlinkdir-main /d /E /H /C /I /Y 
-rmdir freesearcherlinkdir-main /s /Q
-del freesearcherlinkdir-main.zip /s /Q 
-del README.md /Q
-attrib +h *.bat
-attrib +h *.sh
+curl -L https://github.com/TheFreeWeb/freesearcherlinkdir/archive/refs/heads/main.zip > freesearcherlinkdir-main.zip >nul 2>&1
+tar xf freesearcherlinkdir-main.zip >nul 2>&1
+Xcopy freesearcherlinkdir-main /d /E /H /C /I /Y >nul 2>&1
+rmdir freesearcherlinkdir-main /s /Q >nul 2>&1
+del freesearcherlinkdir-main.zip /s /Q >nul 2>&1
+del README.md /Q >nul 2>&1
+attrib +h *.bat >nul 2>&1
+attrib +h *.sh >nul 2>&1
 echo Update installed
 cd /d %~dp0
 cmd.exe
