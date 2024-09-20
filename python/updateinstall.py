e57c8731e31f35654c4fa649100be8a1788bc11b
Martycat111 <walfie308@gmail.com>
2024-09-21 06:58:06 +1200

    Update updateinstall.py

--- python/updateinstall.py
+++ python/updateinstall.py
@@ -2,5 +2,5 @@ import requests, os
 r = requests.get('https://raw.githubusercontent.com/Martycat111/Freesearcher-python-download/refs/heads/main/freesearcher-python.py')
 if r.status_code == 200:
     try: open(os.path.join(os.path.dirname(__file__), 'freesearcher-python.py'), 'wb').write(r.content)
-    except Exception as e: print(e)
+    except Exception as a: print(a)
 else: print(r.status_code)
