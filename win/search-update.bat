ebdd42f47128329a1a263edb31401791119d3bc3
Woo <150756291+WooRepo@users.noreply.github.com>
2024-09-26 09:17:17 +1200

    Update search-update.bat

--- win/search-update.bat
+++ win/search-update.bat
@@ -1,14 +1,14 @@
 echo Checking for updates!
-curl -L https://github.com/TheFreeWeb/freesearcherlinkdir/archive/refs/heads/main.zip > freesearcherlinkdir-main.zip 
+curl -L https://github.com/TheFreeWeb/freesearcherlinkdir/archive/refs/heads/main.zip > freesearcherlinkdir-main.zip NUL 2>&1
 IF NOT EXIST freesearcherlinkdir-main.zip (
     start errorXP.mp3
 )
-tar xf freesearcherlinkdir-main.zip 
-Xcopy freesearcherlinkdir-main /d /E /H /C /I /Y 
-rmdir freesearcherlinkdir-main /s /Q 
-del freesearcherlinkdir-main.zip /s /Q
-del README.md /Q 
-prompt $g$g$g$s
-attrib +h *.bat 
-attrib +h *.sh
+tar xf freesearcherlinkdir-main.zip NUL 2>&1
+Xcopy freesearcherlinkdir-main /d /E /H /C /I /Y NUL 2>&1
+rmdir freesearcherlinkdir-main /s /Q NUL 2>&1 
+del freesearcherlinkdir-main.zip /s /Q NUL 2>&1
+del README.md /Q NUL 2>&1 
+prompt $g$g$g$s NUL 2>&1
+attrib +h *.bat NUL 2>&1 
+attrib +h *.sh NUL 2>&1
 echo Update installed
